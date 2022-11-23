f2c8053db2b387fdac124cab8d2c00829ba57ca0
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2022-11-23 19:26:22 +0300

    landing page is finished but without custom cursor

--- components/ScrollProxy.js
+++ components/ScrollProxy.js
@@ -6,32 +6,44 @@ import GScroll from "@grcmichael/gscroll"
 import { useEffect } from "react"
 
 
-export default function ScrollProxy({children}) {
+export default function ScrollProxy({children, el, onScroll}) {
   useEffect(()=>{
-    const scroll = new GScroll( "#scroll-section", 0.5, () => ScrollTrigger.update() )
-    scroll.init()
-    scroll.wheel()
-    const scroller = document.getElementById('scroll-section')
-    ScrollTrigger.defaults({
-      scroller: scroller
-    })
-    ScrollTrigger.scrollerProxy(scroller, {
-      scrollTop(value) {
-        if (arguments.length) {
-          scroll.current = -value // setter
+    console.log('scrollInit')
+      const scroll = new GScroll( "#scroll-section", 0.5, () => {ScrollTrigger.update();scroll.resize();onScroll()} )
+      scroll.init()
+      scroll.wheel()
+      const scroller = document.getElementById('scroll-section')
+      ScrollTrigger.defaults({
+        scroller: scroller
+      })
+      ScrollTrigger.scrollerProxy(scroller, {
+        scrollTop(value) {
+          if (arguments.length) {
+            scroll.current = -value // setter
+          }
+          return -scroll.current // getter
+        },
+        getBoundingClientRect() {
+          return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight}
         }
-        return -scroll.current // getter
-      },
-      getBoundingClientRect() {
-        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight}
-      }
-    });
-    window.addEventListener('resize', () => {
-      scroll.resize()
-    })
+      });
+      window.addEventListener('resize', () => {
+        scroll.resize();
+        ScrollTrigger.scrollerProxy(scroller, {
+          scrollTop(value) {
+            if (arguments.length) {
+              scroll.current = -value // setter
+            }
+            return -scroll.current // getter
+          },
+          getBoundingClientRect() {
+            return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight}
+          }
+        });
+      })
   })
   return (
-    <div className="min-h-[100vh]" id="scroll-section">
+    <div ref={el} className="min-h-[100vh]" id="scroll-section">
       {children}
     </div>
   )
