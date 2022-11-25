9944f180e92bb9722b6b4b97e1c6db7c2d181b54
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2022-11-25 15:36:38 +0300

    added scroll refresh to be safe

--- components/LocoProxy.js
+++ components/LocoProxy.js
@@ -3262,6 +3262,7 @@ var Native = /*#__PURE__*/function () {
   
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
+  window.addEventListener("resize", ()=>setTimeout(ScrollTrigger.refresh(), 1000))
 
   })
   return (
