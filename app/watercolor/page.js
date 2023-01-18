12db5bf916366fb04e86ca29e6dd1950d1eee8fb
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2023-01-18 18:04:45 +0300

    testing out animations

--- app/watercolor/page.js
+++ app/watercolor/page.js
@@ -15,13 +15,22 @@ import titleBgGradient from "../../public/titleBgGradient.svg"
 import canvasMaskShadow from "../../public/canvasMaskShadow.svg"
 import watercolorSplash from "../../public/watercolor splash low contrast 3.png"
 import watercolorSplash0 from "../../public/watercolor splash.png"
+import watercolorSplash1 from "../../public/watercolor splash low contrast 2.png"
 
 
 export default function WatercolorHome() {
   const {app, gsap} = useContext(PIXIGSAPcontext)
   useEffect(()=>{
     if (app) {
-      
+      //array of functions to run on resize
+      let runOnResize = []
+
+      //run all functions in runOnResize when size of the body changes
+      new ResizeObserver(()=>{
+        runOnResize.forEach((a)=>{
+          if (a) a();
+        })}).observe(document.body)
+
 
       //images into sprites 
       const heroBgSprite = Sprite.from(heroBg.src)
@@ -36,127 +45,58 @@ export default function WatercolorHome() {
       
       //bg scale for positioning
       let bgScale
-
-
-      //colored rectangles on whole screen
-      const splashColor = new Graphics()
-      rectangleOnWholeScreen(splashColor, 0xFFFF00)
-
-      addColoredSlpash(watercolorSplash, 0xFFFF00).then((sprite) => {
-        sprite.anchor.set(0.5, 0.5)
-      })
       
+      //const splash1Sprite = addColoredSlpash(watercolorSplash, 0xFFFF00)
+      //function positionSplash1Sprite() {
+      //  splash1Sprite.then((sprite)=> sprite.anchor.set(0.5,0.5))
+      //}
+      //positionSplash1Sprite()
+      //runOnResize.push(positionSplash1Sprite)
       
-      const splashColor2 = new Graphics()
-      rectangleOnWholeScreen(splashColor2, 0x925FFF)
+      let position = {position:0}
+      gsap.fromTo(position, {position:0}, {position:app.screen.width, repeat:-1, duration:1, ease:"linear"})
+      let tint = {tint:"0x00F000"}
+      testTrail()
+      function testTrail() {
+        setInterval((()=> {
+          let spr = Sprite.from(watercolorSplash1.src)
+          spr.tint = tint.tint
+          spr.anchor.set(0.5, 0.5)
+          spr.width = 300
+          spr.height = 300
+          spr.rotation = Math.random()*1000
+          spr.position.y = 500
+          spr.position.x = position.position
+          let tl = gsap.timeline()
+          tl.fromTo(spr, {alpha: 0}, {alpha: 0.5,duration: 0.1})
+          tl.to(spr, {alpha:0, ease:"power2", duration:1})
+          app.stage.addChild(spr)
+          console.log(position)
+        }), 30)
+      }
       
-      const splashColor3 = new Graphics()
-      rectangleOnWholeScreen(splashColor3, 0x6A015A)
-      
+      //addColoredSlpash(watercolorSplash, 0x925FFF)
+      //
+      //addColoredSlpash(watercolorSplash0, 0x6A015A)
       
       canvasSprite2.mask = canvasMaskShadowSprite
-      titleBgGradientSprite.mask = guasheLIne2Sprite
-
-
-      const title = new Text("Watercolor", { 
-        fontSize:(app.screen.width*0.09),
-        fontFamily:"Permanent marker",
-        fill:"white"
-      })
-
-      const scroll = new Text("Scroll", { 
-        fontSize:(app.screen.width*0.03+15),
-        fontFamily:"Acme",
-        fill:"white"
-      })
-
-
-      const text = new Container()
-      text.width = app.screen.widht;
-      text.height = app.screen.height;
-      text.pivot.x = text.width / 2;
-      text.pivot.y = text.height / 2;
-      text.addChild(title)
-      text.addChild(scroll)
-      canvasSprite.mask = scroll, title
+      titleBgGradientSprite.mask = guasheLIne2Sprite 
       
+      app.stage.addChild(canvasSprite)
       
-      //app.stage.addChild(watercolorSplashSprite0)
-      //app.stage.addChild(canvasMaskShadowSprite)
-      //app.stage.addChild(watercolorSplashSprite)
-      //app.stage.addChild(watercolorSplashSprite2)
-      //app.stage.addChild(guasheLIne2Sprite)
-      //app.stage.addChild(heroBgSprite)
-      //app.stage.addChild(splashColor)
-      //app.stage.addChild(splashColor2)
-      //app.stage.addChild(canvasSprite2)
-      //app.stage.addChild(splashColor3)
-      //app.stage.addChild(titleBgGradientSprite)
-      //app.stage.addChild(text)
-      //app.stage.addChild(canvasSprite)
-      function resizeToCenter( sprite ) {
-        sprite.anchor.set(0.5)
-        sprite.position.set(app.screen.width/2, app.screen.height/2)
-      }
-      function resizeToCover( sprite ) {
-        bgScale = Math.max(app.screen.width/sprite.texture.width, app.screen.height/sprite.texture.height)
-        sprite.scale.set(bgScale, bgScale);
-        sprite.anchor.set(0.5)
-        sprite.position.set(app.screen.width/2, app.screen.height/2)
-      }
-      function resizeToFillObject( sprite, object ) {
-        sprite.width = object.width
-        sprite.height = object.height
-      }
+      //replace pixi ticker with gsap ticker
       app.ticker.stop()
       gsap.ticker.add((time, deltaTime, frames)=>{
-        resizeToCover(heroBgSprite)
-        resizeToCover(canvasSprite)
-        resizeToCover(canvasSprite2)
-        resizeToCenter(title)
-        resizeToCenter(guasheLIne2Sprite)
-        resizeToCenter(titleBgGradientSprite)
-        resizeToFillObject(titleBgGradientSprite, guasheLIne2Sprite)
-        title.style = {
-          fontSize:(app.screen.width*0.09),
-          fontFamily:"Permanent marker",
-          fill:"white"
-        }
-        scroll.style = {
-          fontSize:(app.screen.width*0.03+15),
-          fontFamily:"Acme",
-          fill:"white"
-        }
         
-        splashColor.width = app.screen.width
-        splashColor.height = app.screen.height
-        splashColor2.widht = app.screen.width
-        splashColor2.height = app.screen.height
-        watercolorSplashSprite0.anchor.set(0.5,0.5)
-        watercolorSplashSprite0.width = app.screen.width*0.27
-        watercolorSplashSprite0.height = app.screen.height*0.12
-        watercolorSplashSprite0.position.set(app.screen.width/2, app.screen.height*0.8)
-        watercolorSplashSprite.anchor.set(0.5,0.5)
-        watercolorSplashSprite.width = app.screen.width
-        watercolorSplashSprite.height = app.screen.height
-        watercolorSplashSprite2.width = app.screen.width
-        watercolorSplashSprite2.height = app.screen.height
-        watercolorSplashSprite2.anchor.set(0.5,0.5)
-        watercolorSplashSprite2.position.set(app.screen.width, app.screen.height)
-        guasheLIne2Sprite.width = app.screen.width * 0.73
-        guasheLIne2Sprite.height = app.screen.width * 0.19 
-        guasheLIne2Sprite.position.set(app.screen.width/2, app.screen.height*0.51)
-        titleBgGradientSprite.position.set(app.screen.width/2, app.screen.height*0.51)
-        canvasMaskShadowSprite.width = app.screen.width
-        canvasMaskShadowSprite.height = app.screen.height
         app.ticker.update()
-
-        
       })
+      
+      
+      //functions
       async function addColoredSlpash(mask, color) {
         const texturePromise = Assets.load(mask.src)
         return texturePromise.then((texture)=>{
-          const sprite = Sprite.from(texture)
+          const sprite = Sprite.from(texture)          
           const rect = new Graphics()
           rectangleOnWholeScreen(rect, color).mask = sprite
           app.stage.addChild(sprite)
@@ -164,6 +104,7 @@ export default function WatercolorHome() {
           return sprite
         })
       }
+
       function rectangleOnWholeScreen(graphicsObject, color) {
         if(color) {
           graphicsObject.beginFill(color);
@@ -174,6 +115,22 @@ export default function WatercolorHome() {
         }
         return graphicsObject
       }
+      
+
+      //positioning functions
+      function coverScreen( sprite ) {
+        bgScale = Math.max(app.screen.width/sprite.texture.width, app.screen.height/sprite.texture.height)
+        sprite.scale.set(bgScale, bgScale);
+        sprite.anchor.set(0.5)
+        sprite.position.set(app.screen.width/2, app.screen.height/2)
+        window.addEventListener( 'resize', (sprite) => {
+          bgScale = Math.max(app.screen.width/sprite.texture.width, app.screen.height/sprite.texture.height)
+          sprite.scale.set(bgScale, bgScale);
+          sprite.anchor.set(0.5)
+          sprite.position.set(app.screen.width/2, app.screen.height/2)
+        })
+      }
+
     }
   }, [app])
   return(<></>)
